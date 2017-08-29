c4b99928f8c23b696957055340e0f6f1ef6c7845
Tygran Gorelov <31432827+T-Strannik@users.noreply.github.com>
2017-08-29 22:11:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  	$('.slider').slick({
+  		infinite: true,
+  		slidesToShow: 3,
+  		slidesToScroll: 3
+  	});
+});
+
+//http://kenwheeler.github.io/slick/  - 2слайдер
